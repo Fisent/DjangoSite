082658f17b2582ac4a5b455b9e370644ac77abdc
Fisent <lukzieniewicz@gmail.com>
2017-04-01 03:40:36 +0200

    small letter

--- slowkasite/words/models.py
+++ slowkasite/words/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Word(models.Model):
-    Word_eng = models.CharField(max_length = 200)
-    Word_pol = models.CharField(max_length = 200)
+    word_eng = models.CharField(max_length = 200)
+    word_pol = models.CharField(max_length = 200)
     word_level = models.IntegerField(default = 0)
